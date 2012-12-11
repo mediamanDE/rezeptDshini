5394e7f36c57468f03d926078490b9a0ba17e598
tinkerding <gude@tinkerding.de>
2012-12-11 11:03:24 +0100

    Zusätzlichen Programmcode zur Hardwaresteuerung eingefügt.

--- sketches/sketch_rezept_dshini/sketch_rezept_dshini.ino
+++ sketches/sketch_rezept_dshini/sketch_rezept_dshini.ino
@@ -17,15 +17,61 @@
 #include <Ethernet.h>
 #include <Adafruit_Thermal.h>
 #include <SoftwareSerial.h>
+#include "pitches.h"
 
 #define FONT_END7F //chars: 0x20-0xFF
+
+#define LED_1 7
+#define LED_2 8
+#define LED_3 9
+#define LED_4 11
+#define LED_5 12
+#define LED_6 13
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[6] = {  0x90, 0xA2, 0xDA, 0x0D, 0x0F, 0xB2 };
 IPAddress server(10,15,20,110);
-int printer_RX_Pin = 5;  // This is the green wire
-int printer_TX_Pin = 6;  // This is the yellow wire
+int printer_RX_Pin = 22;  // This is the green wire
+int printer_TX_Pin = 23;  // This is the yellow wire
+
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4,4,4,4,4 };
+  
+// define Tone pin
+int tonePin = 49;
+  
+  
+const int buttonPinGo = 24;     // the number of the pushbutton pin
+
+const int buttonPinRational_1 = 26;     // the number of the pushbutton pin
+const int buttonPinRational_2 = 28;     // the number of the pushbutton pin
+const int buttonPinRational_3 = 30;     // the number of the pushbutton pin
+const int buttonPinRational_4 = 32;     // the number of the pushbutton pin
+const int buttonPinRational_5 = 34;     // the number of the pushbutton pin
+const int buttonPinRational_6 = 36;     // the number of the pushbutton pin
+
+
+const int buttonPinEmotional_1 = 27;     // the number of the pushbutton pin
+const int buttonPinEmotional_2 = 29;     // the number of the pushbutton pin
+const int buttonPinEmotional_3 = 31;     // the number of the pushbutton pin
+const int buttonPinEmotional_4 = 33;     // the number of the pushbutton pin
+
+
+int buttonStateGo = 0;         // variable for reading the button status
+int buttonStateRational = 0;         // variable for reading the button status
+int buttonStateEmotional = 0;         // variable for reading the button status
+
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;
+  
+  
 
 Adafruit_Thermal printer(printer_RX_Pin, printer_TX_Pin);
 
@@ -38,6 +84,10 @@ String preparation = "";
 String line = "";
 boolean startPrint = false;
 boolean printItToPrinterToo = true;
+boolean specialChar = false;
+char specialCharChar;
+
+
 
 
 // Initialize the Ethernet client library
@@ -45,7 +95,39 @@ boolean printItToPrinterToo = true;
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 void setup() {
+    
+  pinMode(LED_1, OUTPUT);  // LED is an OUTPUT 
+  pinMode(LED_2, OUTPUT);  // LED is an OUTPUT 
+  pinMode(LED_3, OUTPUT);  // LED is an OUTPUT 
+  pinMode(LED_4, OUTPUT);  // LED is an OUTPUT 
+  pinMode(LED_5, OUTPUT);  // LED is an OUTPUT 
+  pinMode(LED_6, OUTPUT);  // LED is an OUTPUT 
+  
+  pinMode(buttonPinGo, INPUT);     
+  
+  pinMode(buttonPinRational_1, INPUT);
+  pinMode(buttonPinRational_2, INPUT);
+  pinMode(buttonPinRational_3, INPUT);
+  pinMode(buttonPinRational_4, INPUT);
+  pinMode(buttonPinRational_5, INPUT);
+  pinMode(buttonPinRational_6, INPUT);
+  
+  pinMode(buttonPinEmotional_1, INPUT);
+  pinMode(buttonPinEmotional_2, INPUT);
+  pinMode(buttonPinEmotional_3, INPUT);
+  pinMode(buttonPinEmotional_4, INPUT);
+
+  
+  
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   if(printItToPrinterToo){
@@ -78,10 +160,16 @@ void setup() {
     // kf you didn't get a connection to the server:
     Serial.println("connection failed");
   }
+  
+ 
+  
 }
 
-boolean specialChar = false;
-char specialCharChar;
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 void loop()
 {
   // if there are incoming bytes available 
@@ -171,4 +259,167 @@ void loop()
       ;
   }
 }
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void zumEssen() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(tonePin, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(tonePin);
+  }
+}
+
+
+
+// Ende Loop
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+void fadeAussen()  { 
+ 
+  analogWrite(LED_1, brightness);   
+  analogWrite(LED_2, brightness);
+  analogWrite(LED_3, brightness);
+  analogWrite(LED_4, brightness);
+  analogWrite(LED_5, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ; 
+  }     
+  // wait for 30 milliseconds to see the dimming effect    
+  delay(30);                            
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+void fadeInnen()  { 
+ 
+  analogWrite(LED_6, brightness); 
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ; 
+  }     
+  // wait for 30 milliseconds to see the dimming effect    
+  delay(30);                            
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+void lauflichtAussen()  { 
+ 
+  do 
+  {
+    brightness = brightness + fadeAmount;
+    analogWrite(LED_1, brightness);   
+  }
+  while (brightness < 255);
+
+  do 
+  {
+    brightness = brightness + fadeAmount;
+    analogWrite(LED_2, brightness);   
+  }
+  while (brightness < 255);
+
+  do 
+  {
+    brightness = brightness + fadeAmount;
+    analogWrite(LED_3, brightness);   
+  }
+  while (brightness < 255);
+
+  do 
+  {
+    brightness = brightness + fadeAmount;
+    analogWrite(LED_4, brightness);   
+  }
+  while (brightness < 255);
+
+  do 
+  {
+    brightness = brightness + fadeAmount;
+    analogWrite(LED_5, brightness);   
+  }
+  while (brightness < 255);
+
+////////
+  
+  delay (1000);
+  
+  
+////////
+
+  do 
+  {
+    brightness = brightness - fadeAmount;
+    analogWrite(LED_1, brightness);   
+  }
+  while (brightness > 0);
+
+  do 
+  {
+    brightness = brightness - fadeAmount;
+    analogWrite(LED_2, brightness);   
+  }
+  while (brightness > 0);
+
+  do 
+  {
+    brightness = brightness - fadeAmount;
+    analogWrite(LED_3, brightness);   
+  }
+  while (brightness > 0);
+
+  do 
+  {
+    brightness = brightness - fadeAmount;
+    analogWrite(LED_4, brightness);   
+  }
+  while (brightness > 0);
+
+  do 
+  {
+    brightness = brightness - fadeAmount;
+    analogWrite(LED_5, brightness);   
+  }
+  while (brightness > 0);
+  
+                           
+}
+
 
